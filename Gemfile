3cc6b4bdfc177845467e5db583afa63a83f42a98
Julien 'Lta' BALLET <elthariel@gmail.com>
2013-05-06 20:56:23 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
+
+gem "middleman", "~>3.0.13"
+gem 'rb-inotify', '~> 0.9'
+gem 'compass'
+gem 'flowtime-rails', path: '/home/lta/code/ruby/flowtime-rails'
+
---
+++ source/javascripts/all.js
@@ -0,0 +1,37 @@
+//= require 'jquery'
+//= require 'flowtime'
+//= require 'highlight.pack'
+
+$(function() {
+    Flowtime.showProgress(true)
+    Flowtime.parallaxInPx(true);
+    // Flowtime.fragmentsOnSide(true);
+    // Flowtime.fragmentsOnBack(true);
+    Flowtime.useHistory(false);
+    // Flowtime.slideInPx(true);
+    // Flowtime.sectionsSlideToTop(true);
+    Flowtime.gridNavigation(false);
+    // Flowtime.useOverviewVariant(true);
+
+    Flowtime.start();
+});
+
+$(function() {
+    hljs.tabReplace = '  ';
+    hljs.initHighlightingOnLoad();
+});
+
+WebFontConfig = {
+    google: { families: ['Ubuntu:500,500italic:latin',
+                         'Special+Elite::latin',
+                         'Pacifico::latin',
+                         'Open+Sans:400,600:latin' ] }};
+(function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
