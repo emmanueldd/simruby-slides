11e56e80958e1e314403e019bab8b34a062f0f7f
Emmanuel <e.derozin@gmail.com>
2019-03-04 14:02:54 +0100

    update

--- config.rb
+++ config.rb
@@ -2,14 +2,11 @@
 # Compass
 ###
 
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
-
 # Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+compass_config do |config|
+ config.output_style = :compact
+ config.line_comments = false
+end
 
 ###
 # Page options, layouts, aliases and proxies
@@ -28,10 +25,9 @@
 #   page "/admin/*"
 # end
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
@@ -40,6 +36,8 @@
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
+# Reload the browser automatically whenever files change
+
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
@@ -47,7 +45,9 @@
 #   end
 # end
 
-set :layout, 'layout.haml'
+activate :directory_indexes
+
+set :build_dir, "tmp"
 
 set :css_dir, 'stylesheets'
 
@@ -58,22 +58,17 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :cache_buster
+  # activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
 
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
   # Or use a different image path
-  # set :http_path, "/Content/images/"
+  # set :http_prefix, "/Content/images/"
 end
